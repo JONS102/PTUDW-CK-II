8feaf7387d952c577137b8cdfcac8f9494b1dbc7
JONS102 <nguyenchinhngoc1001@gmail.com>
2025-05-25 16:41:11 +0700

    câu 2

---
+++ controllers/recipeControllers.js
@@ -0,0 +1,9 @@
+let controller = {};
+let models = require('../models');
+let sequelize = require('sequelize');
+let Op = sequelize.Op;
+controller.search = (keyword) => {};
+controller.getById = (id) => {};
+
+controller.getAll = () => {};
+module.exports = controller;
---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname + '/public'));
+//ĐỊnh nghĩa cho ứng dụng express sử dụng View Engine 
+const { engine } = require('express-handlebars');
+app.engine('hbs', engine({
+    extname: '.hbs',
+    defaultLayout: 'layout',
+    layoutsDir: __dirname + '/views/layouts'
+}));
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+//Tạo database
+app.get('/sync', (req, res) => {
+    let models = require('./models');
+    models.sequelize.sync().then(() => {
+        res.send('Database synchronized successfully');
+    });
+}); // tạo table trong db
+
+app.set('view engine', 'hbs');
+//Định nghĩa các route hiển thị 3 trang khác nhau
+app.get('/', (req, res) => {
+    res.render('index');
+});
+app.use('/recipes', require('./routes/recipeRoute')); // khi yêu cầu recipes, chuyển tới route
+
+
+
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get(('port')), () => {
+    console.log(`Server running on port ${app.get('port')}`);
+});
